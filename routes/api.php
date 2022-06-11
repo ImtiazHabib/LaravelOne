7b40362fa5ab28cfc71e50ed55e51013b88dbac9
ImtiazHabib <imtiazhabib7@gmail.com>
2022-06-11 15:31:43 +0600

    Api Authentication using Sanctum, And ********** First version of the project has done ---------***************************

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,9 +21,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // ---------------- Course ----------------- CRUD apis start
 
-// get all course informations
+Route::group(['middleware' => 'auth:sanctum'], function(){
+    //All secure URL's
+    
+    // get all course informations
 Route::get('/allcourse','App\Http\Controllers\Api\ApiController@allcourse');
-
 // create new course -- for now only three items
 Route::post('/create_course','App\Http\Controllers\Api\ApiController@create_course');
 
@@ -32,7 +35,9 @@ Route::put('/edit_course','App\Http\Controllers\Api\ApiController@edit_course');
 // delete specific course 
 Route::delete('/delete_course/{id}','App\Http\Controllers\Api\ApiController@delete_course');
 
+    });
 
+Route::post("/login",[UserController::class,'index']);
 
 
 
